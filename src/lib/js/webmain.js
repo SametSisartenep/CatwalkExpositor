024db275298e68ddac82cd4e139a995f7eaac72e
rodri <rgl@antares-labs.eu>
2019-10-23 19:37:27 +0200

    expositor almost fully operational.

--- src/lib/js/webmain.js
+++ src/lib/js/webmain.js
@@ -1,11 +1,26 @@
+const DEG = 0.0174532925199;
+
 var source, viewport;
 var dctx;
+var expoconfig;
 
 source = document.getElementById("source");
 viewport = document.getElementById("viewport");
 viewport.width = document.body.clientWidth;
 viewport.height = document.body.clientHeight;
 dctx = viewport.getContext("2d");
+
+function
+Dx(rect)
+{
+	return rect.maxx-rect.minx;
+}
+
+function
+Dy(rect)
+{
+	return rect.maxy-rect.miny;
+}
 
 function
 processframe()
@@ -15,7 +30,7 @@ processframe()
 	var npixel;
 	var i;
 
-	dctx.drawImage(source, 0, 0, viewport.width, viewport.height);
+	dctx.drawImage(source, expoconfig.clipr.minx, expoconfig.clipr.miny, Dx(expoconfig.clipr), Dy(expoconfig.clipr), 0, 0, viewport.width, viewport.height);
 	frame = dctx.getImageData(0, 0, viewport.width, viewport.height);
 	npixel = frame.data.length / depth;
 	for(i = 0; i < npixel; i++){
@@ -29,7 +44,7 @@ processframe()
 function
 play()
 {
-	if(source.paused || source.ended)
+	if(source.paused || source.ended || !expoconfig)
 		return;
 	processframe();
 	setTimeout(play, 0);
@@ -38,5 +53,12 @@ play()
 source.addEventListener("play", play, false);
 
 ipcRenderer.on("new-config", (e, data) => {
-	console.log("new config: ", data);
+	expoconfig = data;
+	source.src = data.video;
+	if(expoconfig.rotation !== 0){
+		dctx.resetTransform();
+		dctx.translate(viewport.width/2, viewport.height/2);
+		dctx.rotate(expoconfig.rotation*DEG);
+		dctx.translate(-viewport.width/2, -viewport.height/2);
+	}
 });
--- src/main.js
+++ src/main.js
@@ -4,12 +4,23 @@ const net = require("net");
 const { app, BrowserWindow } = require("electron");
 const { ipcMain, dialog } = require("electron");
 
+const DBPATH = path.join(__dirname, "db/");
+const CFGFILE = path.join(DBPATH, "config.json");
+
 let mainwindow;
-let cmdpipe;
+let cmdsock;
+
+function
+isemptyjson(json)
+{
+	return Object.getOwnPropertyNames(json).length <= 0;
+}
 
 function
 createwindow()
 {
+	var cfg0;
+
 	mainwindow = new BrowserWindow({
 		autoHideMenuBar: true,
 		show: false,
@@ -21,6 +32,18 @@ createwindow()
 	mainwindow.on("closed", () => {
 		mainwindow = null;
 	});
+	mainwindow.on("show", () => {
+		fs.readFile(CFGFILE, (err, data) => {
+			if(err == null){
+				try{
+					cfg0 = JSON.parse(data.toString());
+				}catch(err){
+					return;
+				}
+				mainwindow.webContents.send("new-config", cfg0);
+			}
+		});
+	});
 	mainwindow.once("ready-to-show", () => {
 		mainwindow.show();
 	});
@@ -31,8 +54,8 @@ app.on("ready", createwindow);
 
 app.on("window-all-closed", () => {
 	if(process.platform !== "darwin"){
-		if(cmdpipe)
-			cmdpipe.close();
+		if(cmdsock)
+			cmdsock.close();
 		app.quit();
 	}
 });
@@ -42,16 +65,17 @@ app.on("activate", () => {
 		createwindow();
 });
 
-cmdpipe = net.createServer((stream) => {
+cmdsock = net.createServer((stream) => {
 	stream.on("data", (data) => {
 		var newcfg;
 
 		try{
 			newcfg = JSON.parse(data.toString());
-		}catch(e){
-			return;
+		}catch(err){
+			return;	/* not a JSON */
 		}
+		fs.writeFileSync(CFGFILE, JSON.stringify(newcfg, null, "	"));
 		mainwindow.webContents.send("new-config", newcfg);
 	});
 });
-cmdpipe.listen("/tmp/catwalk.pipe");
+cmdsock.listen("/tmp/catwalk.sock");
